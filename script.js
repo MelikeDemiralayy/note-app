650d165f0d1244d704ea1fd4133cf5c1d99cdd40
mdemiralay <m.demiralay@vitalen.com.tr>
2024-11-14 17:57:56 +0300

    Note cards created and notes saved to localStorage

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-// HTML elementlerini seç
+
 const addBox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
 const closeIcon = popupBox.querySelector("header i");
+addBtn = popupBox.querySelector("button");
+titleTag = popupBox.querySelector("input");
+descTag = popupBox.querySelector("textarea");
+
+
+
+const months = [
+    "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+    
+
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 
 
 
@@ -13,3 +24,34 @@ addBox.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
     popupBox.classList.remove("show");
 });
+
+
+addBtn.addEventListener("click", e => {
+   e.preventDefault();
+   let noteTitle = titleTag.value,
+   noteDesc = descTag.value;
+
+   if(noteTitle || noteDesc){
+    let dateObj = new Date();
+    month = months[dateObj.getMonth()];
+    day = dateObj.getDate();
+    year = dateObj.getFullYear();
+
+   let noteInfo ={
+     title: noteTitle, desciription: noteDesc,
+     date:`${month},${day},${year}`
+
+   }
+       console.log(noteInfo)
+    // console.log(noteDesc,noteTitle)
+    // console.log(month,day, year)
+
+
+    const notes = [];
+    notes.push(noteInfo);
+    localStorage.setItem("notes",JSON.stringify(notes))
+   }
+ 
+});
+
+//32
